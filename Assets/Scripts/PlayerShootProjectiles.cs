85b9b672c2337fc41f4b092be880afb1ca07390e
Javier Vargas <3231javiervargas@gmail.com>
2023-06-26 22:30:59 -0400

    beginning the shooting

---
+++ Assets/Scripts/PlayerShootProjectiles.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootProjectiles : MonoBehaviour
+{
+//     [SerializeFeild] private Transform bullet;
+
+//    private void Awake() {
+//         GetComponent<CharacterAim_Base>().OnShoot += PlayerShootProjectiles_OnShoot;
+//     }
+
+//     private void PlayerShootProjectiles_OnShoot(object sender, CharacterAim_Base.OnShootEventArgs e) {
+//         Instantiate(bullet, e.gunEndPointPosition, Quaternion.identity);
+//     }
+}
